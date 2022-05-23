3633c12677e94069d4b9e5ba5c01d1364582677e
Anna Beatriz <annab_oliver@hotmail.com>
2022-05-22 22:32:12 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double primeiroNumero, segundoNumero;
+
+Console.Write("Digite o primeiro número..: ");
+primeiroNumero = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite o segundo número...: ");
+segundoNumero = Convert.ToDouble(Console.ReadLine()!);
+
+if (primeiroNumero > segundoNumero)
+{
+    Console.WriteLine($"O maior número é {primeiroNumero}");
+}
+else
+{
+    Console.WriteLine($"O maior número é {segundoNumero}");
+}
